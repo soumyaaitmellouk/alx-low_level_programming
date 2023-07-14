7780c4af5ff84377cb2632c4eb5e39685a8f6238
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-14 11:05:15 -0700

    first code C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-gcc puts_function.c -o a.out 
+#include <stdio.h>
+
+int main(void)
+{
+        char str[]="Programming is like building a multilingual puzzle";
+                puts(str);
+                return(0);
+} 
