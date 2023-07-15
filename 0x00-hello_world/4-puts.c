0711c46753ad3b55a4f0acdb9e33b88e9fd33460
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-15 08:02:49 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main is the first function  that is executed when it begins executing
- * return always 0
- * */
+ * main: is the first function  that is executed when it begins executing
+ * return: always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
