981f6e96a8a0609638b154fa51223eb1e81b3b67
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-15 07:42:12 -0700

    first code C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*main function is the first function in your program that is executed when it begins executing*/
-int main(void) 
+/*main is the first function  that is executed when it begins executing*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
