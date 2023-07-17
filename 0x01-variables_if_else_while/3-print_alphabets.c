6e8a35be5654c18c0f6fa31f3144f9a93afa527c
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-17 16:48:06 -0700

    h

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*0 is zero 
+*/
+int main(void)
+{
+int n =  97;
+int m = 65;
+for (n = 97 ; n <= 122; n++)
+{
+putchar(n);
+}
+putchar('\n');
+for (m = 65 ; m <= 90; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
