7e86a47b5b6bdabc15f98f6486c99135c0bd046e
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-17 15:32:33 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: c program print a sentance.
+* Return: Always 0 (Success)
+* 0 is zero 
+*/
+void main()
+{
+int ch;
+ch=='a';
+for (ch= 'a'; ch<= 'z'; ch++)
+{
+putchar(ch);
+}
