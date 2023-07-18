21e4e5c1228ba37dad168dcaddd2b38df1920ce4
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-17 17:16:50 -0700

    for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*0 is zero
+*/
+int main(void)
+{
+int n;
+for (n = 0; n<=9; n++)
+{
+printf ("%d", n);
+}
+printf ("\n");
+return (0);
+}
