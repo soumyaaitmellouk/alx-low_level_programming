314e7505d7614893a510721c4a00f8baebb7ca48
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-17 17:34:34 -0700

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*0 is zero 
+*/
+int main(void)
+{
+int n;
+for (n = 122 ; n >= 97; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
