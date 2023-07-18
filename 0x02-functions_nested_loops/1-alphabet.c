37e9efffed1f21e7ee9b1e441db6768442a9218e
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-18 10:44:03 -0700

    new nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n=97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
