be8e14dea6730e1c2af9b7f391daba60d23fbe84
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-17 17:45:53 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*0 is zero 
+*/
+int main(void)
+{
+int n;
+int m;
+for (n = 48; n <= 57 ; n++)
+{
+putchar(n);
+}
+for (m = 97; m <=102; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
