b7bfac10edd24aea6545d68aec4b41d3416d8e9a
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-18 17:53:52 -0700

    putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_alphabet -print the alphabets in lowercase
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void);
+{
+int m = 1
+while (m <= 10)
+{
+int n = 97;
+while (n <= 122)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+m++
+}
+}
