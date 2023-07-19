4927e8cf7c079498fd56678e539845756588514c
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 05:14:28 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet -print the alphabets in lowercase
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+if (c  <= 97 ) && (c >= 122)
+{
+return (1);
+}
+else if (c <= 65) && (c >= 90)
+{
+return (0);
+}
+}
