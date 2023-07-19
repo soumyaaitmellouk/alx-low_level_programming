6b73d5424fd56104b3d0c3779bd7c262cf49108d
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 15:20:28 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_last_digit - print thelast digit of a number
+*Description: c program print the last digit.
+*@n :le parametre a chercher
+*Return: last digit
+*/
+int print_last_digit(int n)
+{
+int m;
+m = n % 10;
+return (m)
+}
