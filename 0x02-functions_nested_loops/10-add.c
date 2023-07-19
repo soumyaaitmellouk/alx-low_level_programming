3d9f6f8d0d5494b06391686394a93f3dc53baec6
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 16:34:54 -0700

    sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
 *add - add two numbers.
+*Description: calcul the sum of two numbers 
 * @n :number 1
 * @m :number 2
-* Return :the result.
+*Return: the result.
 */
 int add(int n, int m)
 {
