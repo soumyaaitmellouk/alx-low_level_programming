e0219b44e374b554aab2821cebacbe457aa9ad23
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 07:41:36 -0700

    new

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,13 +7,15 @@
 int _islower(int c)
 {
 int m;
+int i = 48;
+int j = 49;
 if (c  <= 97 && c >= 122)
 {
-m = _putchar('1');
+m = _putchar(j);
 }
 else if (c <= 65 && c >= 90)
 {
-m = _putchar('0');
+m = _putchar(i);
 }
 return (m);
 }
