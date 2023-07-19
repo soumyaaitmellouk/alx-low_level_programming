71997a9e13182803be167791ce2a1c47e5a91df8
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 16:57:29 -0700

    vers 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*print_to_98 - afficher les nombres jusqu'à 98.
+*@n :number 1 
+*Return:the result.
+*/
+void print_to_98(int n)
+{
+while (n < 98)
+{
+_putchar (n + 48);
+_putchar (',');
+_putchar (' ');
+n++;
+}
+while (n > 98)
+{
+_putchar (n + 48);
+_putchar (',');
+_putchar (' ');
+n--;
+}
+if ( n == 98)
+{
+_putchar (n + 48);
+}
+}
