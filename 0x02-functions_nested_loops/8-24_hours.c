aab97b6b7feb2361c520feacae9d39f16b19f3e5
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 15:54:42 -0700

    24 h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,14 @@ void jack_bauer(void)
 int i, j, k, l;
 for (i = 0; i <= 2; i++)
 {
-for (j = 0; j <= 3; j++)
+for (j = 0; j <= 9; j++)
 {
 for (k = 0; k <= 5; k++)
 {
-for (l = 0; l <=9; l++)
+for (l = 0; l <= 9; l++)
 {
+if (i >=2 && j >= 4)
+	break;
 _putchar(i + 48);
 _putchar(j + 48);
 _putchar(58);
