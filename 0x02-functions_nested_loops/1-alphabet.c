18dc7251c37009e0302b4c8fabc2b8173178e913
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-18 17:47:26 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
 #include "main.h"
+/**
+*print_alphabet -print the alphabets in lowercase
+*Description: c program print a sentance.
+*Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 int n=97;
 while (n <= 122)
 {
-putchar(n);
+_putchar(n);
 n++;
 }
-putchar('\n');
-return;
+_putchar('\n');
 }
