d0f0d3cd852cfd59f447befa5f6a5a9b20cdc0ad
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-19 16:14:34 -0700

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,15 @@ void times_table(void)
 {
 int i;
 int j;
+int t;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar(i * j);
-_putchar('\n');
+t=i * j;
+return (t);
+_putchar(39);
 }
+_putchar('\n');
 }
 }
