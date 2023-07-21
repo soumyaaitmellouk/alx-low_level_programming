ad006228c0c05caf0d4ee618e7a39e5052dcb331
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-20 17:15:30 -0700

    up

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,13 @@
 */
 int _isupper(int c)
 {
-if (c  >= 97 && c <= 122)
-{
-return (0);
-}
-else if (c >= 65 && c <= 90)
+if (c >= 65 && c <= 90) 
 {
 return (1);
 }
+else
+{
+return (0);
+}
 _putchar('\n');
-return;
 }
