257a106a9b6a436eb653bd5a143aa5b8d506ba30
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-25 15:55:46 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int j = 0;
+	int i;
+
+	while (s[j] != '\0')
+	{
+	j++;
+	}
+	for (i = 0; i < j; i++)
+	{
+		j--;
+		rev = s[i];
+		s[i] = s[j];
+		s[j] = rev;
+	}
+}
