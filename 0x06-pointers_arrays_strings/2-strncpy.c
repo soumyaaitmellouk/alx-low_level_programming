5af841125d44564fb4b649fa5e5c41bc39532cc0
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-27 16:08:19 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,3 +6,14 @@
  * @n: nombre de caractère.
  * Return: le nouveau dest.
  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
