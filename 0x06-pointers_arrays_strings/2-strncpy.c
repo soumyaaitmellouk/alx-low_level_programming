3260048c9fe236e661b7200bdfbe16f3bddc7869
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-07-27 15:59:16 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strncpy - Write a function that copies a string.
+ * @dest: first string.
+ * @src: second string.
+ * @n: nombre de caractère.
+ * Return: le nouveau dest.
+ */
