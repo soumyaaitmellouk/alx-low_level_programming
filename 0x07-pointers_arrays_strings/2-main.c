1e74a884dbbd5f691b974b86fed2f7717c144c36
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-08-01 12:29:30 -0700

    pointer

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char *s = "soumya";
+    char *s = "Hello";
     char *f;
 
     f = _strchr(s, 'l');
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -23,5 +23,5 @@ char *_strchr(char *s, char c)
 			i++;
 		}
 	}
-	return ();
+	return (0);
 }
