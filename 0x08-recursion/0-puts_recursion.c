63dbdcad9f5519aae4ec987afcbbf3b04650c675
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-08-03 16:48:06 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (*(s + 1) != '\0')
+	if (*s)
 	{
-		_putchar(*(s + i));
-		i++;
-		_puts_recursion(*(s + i));
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
+
