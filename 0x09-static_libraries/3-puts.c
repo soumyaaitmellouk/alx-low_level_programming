514c5c12873ff8221270c93235165c13af958142
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-08-05 10:46:20 -0700

    library are difficukte

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include "main.h"
+#include<stdio.h>
 /**
  * main - Entry point
- * Description: C program that prints a sentance
+ *
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *str)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return;
 }
