4ebad3c8dc2294c7d8434103cd52995fffdb8111
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-10-11 02:23:37 -0700

    preprocesseur

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef 3-FUNCTION_LIKE_MACRO.H
 #define 3-FUNCTION_LIKE_MACRO.H
-#define ABS(x) if(x>0)/return(x);/else/return(-x);
+#define ABS(x) if((x)>(0))/return(x);/else/return(-(x));
 #endif
