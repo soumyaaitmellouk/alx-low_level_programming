87ca6b669978e5458d2b36f2c2ddd185e86990fd
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-07 14:43:25 -0800

    task1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,21 @@
-#include "list.h"
+#include "lists.h"
+/**
+ * dlistint_len- returns the number of elements in a linked.
+ * @h: parametr
+ * Return: number of node
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int somme = 0;
+
+        if (h == NULL)
+                return (somme);
+        while (h->prev != NULL)
+                h = h->prev;
+        while (h != NULL)
+        {
+                somme++;
+                h = h->next;
+        }
+        return (somme);
+}	
