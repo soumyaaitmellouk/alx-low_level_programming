1e713ce7cb62386cd3c6fdf5cf9ab5bc696b2768
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-22 08:23:39 -0800

    task1 and 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+* key_index- a function that gives you the index of a key.
+* @key: the key to find the index.
+* @size: size of array.
+* Return: index of key.
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return(hash_djb2(key) % size);
+}
