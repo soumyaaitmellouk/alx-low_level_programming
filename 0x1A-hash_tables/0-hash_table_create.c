006b2d4b54abecb1a2d3a9ea3bdd99f6f2196056
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-22 05:09:22 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
 * hash_table_create- create a hash table.
 * @size: the size of the hash table.
@@ -9,17 +10,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new;
 	unsigned long int count;
 
-	new = malloc(sizeof(hash_table_t *));
+	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
-	new->array = malloc(size * sizeof(hash_table_t));
+	new->array = malloc(size * sizeof(hash_table_t *));
 	if (new->array == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 	for (count = 0; count < size; count++)
-	new->array[count] = NULL
-	return (new)
+		new->array[count] = NULL;
+	return (new);
 }
